91597805f4d500f3fd241fe50949f746bd21a0ac
matskevichAnton <2539247M@student.gla.ac.uk>
2021-07-30 16:18:39 +0100

    chapter 10 solutions

--- rango/views.py
+++ rango/views.py
@@ -1,27 +1,39 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from rango.models import Category
-from rango.models import Page
-from rango.forms import CategoryForm
 from django.shortcuts import redirect
 from rango.forms import PageForm
 from django.urls import reverse
+from rango.models import Page
+from rango.forms import CategoryForm
+from django.shortcuts import redirect
 from rango.forms import UserForm, UserProfileForm
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from datetime import datetime
 
 
 def index(request):
+
     category_list = Category.objects.order_by('-likes')[:5]
     page_list = Page.objects.order_by('-views')[:5]
     context_dict = {}
-    context_dict = {'boldmessage': 'crunchy, creamy, cookie, candy, cupcake!'}
+    context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
-    return render(request, 'rango/index.html', context=context_dict)
-# Create your views here.
-def about(request):
-        context_dict = {'name': 'Anton Matskevich'}
+    visitor_cookie_handler(request)
 
-        return render(request, 'rango/about.html', context=context_dict)
+    return render(request, 'rango/index.html', context=context_dict)
+
+
+def about(request):
+    name = 'Anton Matskevich'
+    context_dict = {}
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    context_dict['name'] = name
+    return render(request, 'rango/about.html', context=context_dict)
 
 
 def show_category(request, category_name_slug):
@@ -30,113 +42,137 @@ def show_category(request, category_name_slug):
         category = Category.objects.get(slug=category_name_slug)
         pages = Page.objects.filter(category=category)
         context_dict['pages'] = pages
-
         context_dict['category'] = category
-    
     except Category.DoesNotExist:
-
         context_dict['category'] = None
         context_dict['pages'] = None
-    
     return render(request, 'rango/category.html', context=context_dict)
 
 
+@login_required
 def add_category(request):
     form = CategoryForm()
-
     if request.method == 'POST':
         form = CategoryForm(request.POST)
-
         if form.is_valid():
-
             form.save(commit=True)
-            return redirect('/rango/')
-
+            return redirect(reverse('rango:index'))
         else:
             print(form.errors)
     return render(request, 'rango/add_category.html', {'form': form})
 
+
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
     except Category.DoesNotExist:
         category = None
-# You cannot add a page to a Category that does not exist...
 
     if category is None:
-        return redirect('/rango/')
+        return redirect(reverse('rango:index'))
+
     form = PageForm()
+
     if request.method == 'POST':
         form = PageForm(request.POST)
+
         if form.is_valid():
             if category:
                 page = form.save(commit=False)
                 page.category = category
                 page.views = 0
                 page.save()
-                
                 return redirect(reverse('rango:show_category', kwargs={'category_name_slug': category_name_slug}))
         else:
             print(form.errors)
+
     context_dict = {'form': form, 'category': category}
     return render(request, 'rango/add_page.html', context=context_dict)
 
+
 def register(request):
-    # Request the context.
-    context = RequestContext(request)
-    cat_list = get_category_list()
-    context_dict = {}
-    context_dict['cat_list'] = cat_list
-    # Boolean telling us whether registration was successful or not.
-    # Initially False; presume it was a failure until proven otherwise!
     registered = False
 
-    # If HTTP POST, we wish to process form data and create an account.
     if request.method == 'POST':
-        # Grab raw form data - making use of both FormModels.
-        user_form = UserForm(data=request.POST)
-        profile_form = UserProfileForm(data=request.POST)
+        user_form = UserForm(request.POST)
+        profile_form = UserProfileForm(request.POST)
 
-        # Two valid forms?
         if user_form.is_valid() and profile_form.is_valid():
-            # Save the user's form data. That one is easy.
             user = user_form.save()
-
-            # Now a user account exists, we hash the password with the set_password() method.
-            # Then we can update the account with .save().
             user.set_password(user.password)
             user.save()
 
-            # Now we can sort out the UserProfile instance.
-            # We'll be setting values for the instance ourselves, so commit=False prevents Django from saving the instance automatically.
             profile = profile_form.save(commit=False)
             profile.user = user
 
-            # Profile picture supplied? If so, we put it in the new UserProfile.
             if 'picture' in request.FILES:
                 profile.picture = request.FILES['picture']
 
-            # Now we save the model instance!
             profile.save()
 
-            # We can say registration was successful.
             registered = True
-
-        # Invalid form(s) - just print errors to the terminal.
         else:
             print(user_form.errors, profile_form.errors)
-
-    # Not a HTTP POST, so we render the two ModelForms to allow a user to input their data.
     else:
         user_form = UserForm()
         profile_form = UserProfileForm()
 
-    context_dict['user_form'] = user_form
-    context_dict['profile_form']= profile_form
-    context_dict['registered'] = registered
+    return render(request, 'rango/register.html',
+                  context={'user_form': user_form, 'profile_form': profile_form, 'registered': registered})
 
-    # Render and return!
-    return render_to_response(
-        'rango/register.html',
-        context_dict,
-        context)
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return redirect(reverse('rango:index'))
+            else:
+                return HttpResponse("Your Rango account is disabled.")
+        else:
+            print(f"Invalid login details: {username}, {password}")
+            return HttpResponse("Invalid login details supplied.")
+    else:
+        return render(request, 'rango/login.html')
+
+
+@login_required
+def restricted(request):
+
+    return render(request, 'rango/restricted.html')
+
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect(reverse('rango:index'))
+
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request, 'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+
+        request.session['last_visit'] = str(datetime.now())
+    else:
+
+        request.session['last_visit'] = last_visit_cookie
+
+
+    request.session['visits'] = visits
+
+
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
